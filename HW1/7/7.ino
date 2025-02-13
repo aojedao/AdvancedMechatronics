2284cda12a424bea64d7c3a315330d4c8021f69e
Santiago Bernheim <sbernheim@impulsedynamics.com>
2025-02-13 15:46:12 -0500

    Fixing 7 to avoid multiple responses on with the instructions.

--- HW1/7/7.ino
+++ HW1/7/7.ino
@@ -1,28 +1,36 @@
+const byte led = 8;
 char HL;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  DDRB |= (1<<DDB0);
-  PORTB ^= (1<<PORTB0);
+  DDRB |= (1 << DDB0);        // Set pin 8 as output
+  PORTB &= ~(1 << PORTB0);    // Initialize LED OFF (comment out if inverse logic)
+  Serial.println("Enter H/h to turn ON or L/l to turn OFF:");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-    Serial.print("Turn ON (H) or Turn OFF (L) the led : ");
-    while(Serial.available() == 0){}
+  if (Serial.available() > 0) {
     HL = Serial.read();
-    Serial.println(HL);
-    if(HL == 'h' || HL == 'H'){
-      PORTB |= (1<<PORTB0);
-      Serial.println("ON");
-    }
-    else if (HL == 'L' || HL == 'l'){
-      PORTB &= ~(1<<PORTB0);
-      Serial.println("OFF");
+    
+    // Skip line endings (ASCII 10 = \n, 13 = \r)
+    if (HL == 10 || HL == 13) return;
+    
+    if (HL == 'H' || HL == 'h') {
+      PORTB |= (1 << PORTB0);  // LED ON
+      Serial.println("LED ON");
+    } 
+    else if (HL == 'L' || HL == 'l') {
+      PORTB &= ~(1 << PORTB0); // LED OFF
+      Serial.println("LED OFF");
+    } 
+    else {
+      Serial.println("Invalid input! Use H/h or L/l");
     }
 
-    else Serial.println("Invalid Input");
-
+    // Clear remaining characters in buffer
+    while (Serial.available() > 0) Serial.read();
+  }
+  else {
+    while(Serial.available()<=0); // Waiting for new serial input.
+  }
 }
