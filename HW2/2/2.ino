2ea3ba2d49bc9fedcf2505b4a19a083629128295
Nishant-ZFYII <nishantpushparaju@gmail.com>
2025-02-15 20:02:31 -0500

    The code is fixed

--- HW2/2/2.ino
+++ HW2/2/2.ino
@@ -9,61 +9,149 @@
 //P10: Segment G
 //P11: Segment E
 
-#define B 4 1<<PD4
+#define A 6 (1<<PD6)
+#define B 4 (1<<PD4)
+#define C 7 (1<<PD7)
+
+
+#define D 9 (1<<PB1)
+#define E 11 (1<<PB3)
+#define F 8 (1<<PB0)
+#define G 10 (1<<PB2)
 #define DP 5 1<<PD5
-#define A 6 1<<PD6
-#define C 7 1<<PD7
-#define F 8 1<<PB0
-#define D 9 1<<PB1
-#define G 10 1<<PB2
-#define E 11 1<<PB3
+
 
 void setup(){
   DDRD |= (B11110000);
   DDRB |= B001111;
+  PORTB = B00000000;
   
 }
-
-void zero(void) {
+void all_off(){
+	PORTB &= B000000;//set all to LED's LOW
+  	PORTD &= B00000000;
+}
+void zero(bool frwd_flag) {
   //ABCDEF
-  PORTD = ((1<<PD4) | (1<<PD6) | (1<<))
+  if(frwd_flag){
+      PORTD |= ((1<<PD4) | (1<<PD6) | (1<<PD7) ); // SET BAC
+      PORTB |= ((1<<PB1) | (1<<PB3) | (1<<PB0)); // SET DEF
+  }
+  else{
+    PORTD |= ((1<<PD6));//ON A
+    PORTB |= ( (1<<PB1)| (1<<PB3) | (1<<PB0));//ON DEF
+  }
 }
 
-void one(void) {
+void one(bool frwd_flag) {
   //BC
+  if(frwd_flag){
+    PORTB &= ~( (1<<PB1)| (1<<PB3) | (1<<PB0)); //OFF DEF
+    PORTD &= ~((1<<PD6)); //OFF A
+  }
+  else{
+    PORTD &= ~((1<<PD6));//OFF A
+    PORTB &= ~((1<<PB1)|(1<<PB2)|(1<<PB3));//OFF GED
+    PORTD |= (1<<PD7);//ON C
+  }
 }
 
-void two(void) {
+void two(bool frwd_flag) {
   //ABGED
+  if(frwd_flag){
+    PORTD &= ~(1<<PD7);  //OFF C
+    PORTB |= ((1<<PB2)| (1<<PB3)|(1<<PB1)); //ON DEG
+    PORTD |= ((1<<PD6)); // ON A
+  }
+  else{
+    PORTD &= ~(1<<PD7);//OFF C
+    PORTB |= (1<<PB3);//ON E
+  }
 }
 
-void three(void) {
+void three(bool frwd_flag) {
   //ABGCD
-}
-
-void four(void) {
-  //BCFG
-}
-
-void five(void) {
-  //AFGCD
-}
-
-void six(void) {
-  //ACDEFGG
-}
-
-void seven(void) {
-  //ABC
-}
-
-void eight(void) {
-  //ABCDEFG
-}
-
-void nine(void) {
-  //ABCDFG
+  if(frwd_flag){
+    PORTB &= ~(1<<PB3);//OFF E
+    PORTD |= (1<<PD7);//ON C
+  }
+  else{
+    PORTD |= (1<<PD6);//ON A
+    PORTB &= ~((1<<PB0));//OFF F
+    PORTB |= ((1<<PB1));//ON D
+  }
   
+}
+
+void four(bool frwd_flag) {
+  //BCFG
+  if(frwd_flag){
+    PORTD &= ~(1<<PD6);//OFF A
+    PORTB &= ~(1<<PB1);//OFF D
+    PORTB |=(1<<PB0);//ON F
+    }
+  else{
+    PORTD &= ~(1<<PD6);//OFF A
+    PORTD |= (1<<PD4);;//ON B
+    PORTB &= ~(1<<PB1);//OFF D
+  }
+}
+
+void five(bool frwd_flag) {
+  //AFGCD
+  if(frwd_flag){
+    PORTD &= ~(1<<PD4);//OFF B
+    PORTD |= (1<<PD6);//ON A
+    PORTB |=(1<<PB1);//ON D
+    }
+  else{
+    PORTB &= ~(1<<PB3);//OFF E
+    
+  }
+}
+
+void six(bool frwd_flag) {
+  //ACDEFG
+  if(frwd_flag){
+    PORTB |= (1<<PB3);//ON E
+  }
+  else{
+    PORTD &= ~(1<<PD4);//OFF B
+    PORTD |= ((1<<PD6)|(1<<PD7));//AC
+    PORTB |= ((1<<PB0)|(1<<PB1)|(1<<PB2)|(1<<PB3));//DEFG
+  }
+}
+
+void seven(bool frwd_flag) {
+  //ABC
+  if(frwd_flag){
+    PORTB &= ~((1<<PB1) |(1<<PB3) |(1<<PB0)| (1<<PB2));//OFF DEFG
+    PORTD |= (1<<PD4);//ON B
+  }
+  else{
+    PORTB &= ~((1<<PB1)|(1<<PB3)|(1<<PB0)|(1<<PB2));//OFF DEFG
+  }
+}
+
+void eight(bool frwd_flag) {
+  //ABCDEFG
+  if(frwd_flag){
+    PORTB |=((1<<PB1)|(1<<PB3)|(1<<PB0)|(1<<PB2));//ON DEFG
+  }
+  else{
+    PORTB |= ((1<<PB3));//SET E
+  }
+}
+
+void nine(bool frwd_flag) {
+  //ABCDFG
+  if(frwd_flag){
+    PORTB &= ~(1<<PB3);//OFF E
+  }
+  else{
+    PORTD |= ((1<<PD6)|(1<<PD4)|(1<<PD7));//ABC
+    PORTB |= ((1<<PB0)|(1<<PB1)|(1<<PB2));//DFG
+  }
 }
 
 // Start
@@ -76,60 +164,65 @@ void loop(void)
   while(n >=0 && n<=9){
     switch(n){
       case 0:
-      	zero();
+      	zero(frwd_flag);
       	delay(1000);
       	break;
       case 1 :
-      	one();
+      	one(frwd_flag);
 		delay(1000);
       	break;
       case 2:
-      	two();
+      	two(frwd_flag);
 		delay(1000);
       	break;
       case 3:
-      	three();
+      	three(frwd_flag);
 		delay(1000);
       	break;
       
 
       case 4:
-      	four();
+      	four(frwd_flag);
 		delay(1000);
       	break;
 
       case 5:
-      	five();
+      	five(frwd_flag);
 		delay(1000);
       	break;
       case 6:
-      	six();
+      	six(frwd_flag);
 		delay(1000);
       	break;
       case 7:
-      	seven();
+      	seven(frwd_flag);
 		delay(1000);
       	break;
       
       case 8:
-	    eight();
+	    eight(frwd_flag);
 		delay(1000);
       	break;
       case 9:
-      	nine();
+      	nine(frwd_flag);
 		delay(1000);
       	break;
       }
     //increment n if frwd_flg = 0;
-    if(n == 0) {
-      frwd_flag = 1;
-      delay(2000);
-    }
-    if(n == 9) {
-      frwd_flag = 0;
-      delay(2000);
-    }
     n = frwd_flag?++n:--n;
+    if(n == -1) {
+      frwd_flag = true;
+      all_off();
+      delay(2000);
+      n = 0;
+    }
+    if(n == 10) {
+      frwd_flag = false;
+      all_off();
+      delay(2000);
+      n = 9;
+    }
+    
     }
 }
 
