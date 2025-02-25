aa57cb84940d6b61d54371ecc0c9ec275c1f66d0
Santiago Bernheim <sbernheim@impulsedynamics.com>
2025-02-25 16:54:56 -0500

    fix the encoder testing sketch

--- MiniProject1/Testing/BasicEncoder/BasicEncoder.ino
+++ MiniProject1/Testing/BasicEncoder/BasicEncoder.ino
@@ -10,8 +10,8 @@
 //   Best Performance: both pins have interrupt capability
 //   Good Performance: only the first pin has interrupt capability
 //   Low Performance:  neither pin has interrupt capability
-//Encoder leftEnc(5, 6);
-//Encoder rightEnc(10, 11);
+Encoder leftEnc(10, 11);
+Encoder rightEnc(6, 5);
 
 
 //   avoid using pins with LEDs attached
@@ -19,43 +19,22 @@
 void setup() {
   Serial.begin(9600);
   Serial.println("Basic Encoder Test:");
-
-  pinMode(5, INPUT);
-  pinMode(6, INPUT);
-  pinMode(10, INPUT);
-  pinMode(11, INPUT);
 }
 
 long oldPosition_left  = -999;
 long oldPosition_right  = -999;
 
 void loop() {
- // long newPosition_left = leftEnc.read();
-  //long newPosition_right = rightEnc.read();
+ long newPosition_left = leftEnc.read();
+  long newPosition_right = rightEnc.read();
 
-  Serial.print(digitalRead(5));
-  Serial.print(" ");
-    Serial.print(digitalRead(6));
-  Serial.print(" ");
-    Serial.print(digitalRead(10));
-  Serial.print(" ");
-      Serial.print(digitalRead(11));
-  Serial.println(" ");
   
-
-//  Serial.print(newPosition_left);
-  //Serial.print(" ");
- // Serial.println(newPosition_right);
-
-  /*
-  if (newPosition_left != oldPosition_left) {
+  if (newPosition_left != oldPosition_left || newPosition_right != oldPosition_right) {
     oldPosition_left = newPosition_left;
-    Serial.println(oldPosition_left);
-  }
-
-    if (newPosition_right != oldPosition_right) {
+    Serial.print("l: ");
+    Serial.print(oldPosition_left);
+    Serial.print(" | r: ");
     oldPosition_right = newPosition_right;
     Serial.println(oldPosition_right);
-  }*/
-  //Serial.println("FUCK");
+  }
 }
