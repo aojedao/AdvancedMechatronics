dcee94e0e62fbd6a65806d78115f6d1a90ff52f6
Santiago Bernheim <sbernheim@impulsedynamics.com>
2025-02-25 09:15:10 -0500

    Add more comments for problem 3

--- HW3/HW3-Problem3/HW3-Problem3.ino
+++ HW3/HW3-Problem3/HW3-Problem3.ino
@@ -4,31 +4,31 @@
 
 void setup() {
   // USART initialization
-  UBRR0H = 0; // Baud rate 9600 for 16MHz
-  UBRR0L = 103;
+  UBRR0H = 0; // Set upper 4 bits of UBRR to 0
+  UBRR0L = 103; // Set lower 8 bits of UBRR to 103 (9600 baud)
   UCSR0B |= (1 << TXEN0); // Enable transmitter
-  UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00); // 8-bit data
+  UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00); // 8-bit data frame
 }
 
 void loop() {
-  static unsigned long last = 0;
-  unsigned long now = millis();
-  if (now - last >= 1000) {
-    last = now;
-    sendNumber(now);
+  static unsigned long last = 0; // Store the last time we printed
+  unsigned long now = millis(); // Get current runtime in milliseconds
+  if (now - last >= 1000) { // Check if 1 second has passed
+    last = now; // Update last print time
+    sendNumber(now); // Send runtime to USART
   }
 }
 
 void sendNumber(unsigned long num) {
-  char buffer[10];
+  char buffer[10]; // Buffer to store digits
   int i = 0;
   do {
-    buffer[i++] = num % 10 + '0';
-    num /= 10;
-  } while (num > 0);
+    buffer[i++] = num % 10 + '0'; // Convert digit to ASCII
+    num /= 10; // Move to the next digit
+  } while (num > 0); // Repeat until all digits are processed
   
-  for (int j = i - 1; j >= 0; j--) {
-    while (!(UCSR0A & (1 << UDRE0))); // Wait for buffer
-    UDR0 = buffer[j];
+  for (int j = i - 1; j >= 0; j--) { // Send digits in reverse order
+    while (!(UCSR0A & (1 << UDRE0))); // Wait for UDR0 to be empty
+    UDR0 = buffer[j]; // Send digit
   }
 }
