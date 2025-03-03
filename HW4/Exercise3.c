03d912a5236883a27c691b1c232d24db57e352d5
Santiago Bernheim <sbernheim@impulsedynamics.com>
2025-03-03 15:52:09 -0500

    HW4 first proposal

---
+++ HW4/Exercise3.c
@@ -0,0 +1,36 @@
+/*
+  Blank Simple Project.c
+  http://learn.parallax.com/propeller-c-tutorials 
+*/
+#include "simpletools.h"                      // Include simple tools
+
+#include <stdio.h>
+
+float computeBMI(float weight, float height) {
+    return (weight * 703) / (height * height);
+}
+
+int main() {
+    float feet, inches, weight;
+    float heightInches, bmi;
+
+    // Prompt user for input
+    printf("Enter height (feet): ");
+    scanf("%f", &feet);
+    printf("Enter height (inches): ");
+    scanf("%f", &inches);
+
+    printf("Enter weight (pounds): ");
+    scanf("%f", &weight);
+
+    // Convert height to inches
+    heightInches = feet * 12 + inches;
+
+    // Compute BMI
+    bmi = computeBMI(weight, heightInches);
+
+    // Print result
+    printf("BMI: %.2f\n", bmi);
+
+    return 0;
+}
