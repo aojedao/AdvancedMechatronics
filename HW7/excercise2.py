41a3e97ce72965fe1e3b129a5b293acf5b0ae133
Santiago Bernheim <sbernheim@impulsedynamics.com>
2025-04-25 16:35:17 -0400

    Fixing some minor mistakes and adding some debugging prints

--- HW7/excercise2.py
+++ HW7/excercise2.py
@@ -5,7 +5,10 @@ import random
 def simulate_sensor_readings():
     with open('sensor_readings.txt', 'w') as file:  # Open file to log readings
         start_time = time.time()  # Record start time
+        print("Sensor simulation started. Press CTRL+C to exit.")
+        print("Generating sensor readings...")
         while time.time() - start_time < 60:  # Run for 60 seconds
+            print("...\n")
             sensor1 = random.randint(0, 25)  # Generate random integer for sensor 1
             sensor2 = round(random.uniform(0.00, 5.00), 2)  # Generate random float for sensor 2
             timestamp = time.strftime("%Y-%m-%d %H:%M:%S")  # Get current timestamp
